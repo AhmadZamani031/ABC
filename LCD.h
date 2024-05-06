9f5d0f2022b8fbe8574302b503454784f2990111
Ahmad Zamani <86928029+AhmadZamani031@users.noreply.github.com>
2024-05-06 17:41:15 +0330

    1.0

---
+++ LCD.h
@@ -0,0 +1,25 @@
+#ifndef LCD_H_
+
+#define LCD_H_
+
+#define F_CPU 8000000UL					
+#include <avr/io.h>						
+#include <util/delay.h>					
+
+#define LCD_Data_Dir DDRB				
+#define LCD_Command_Dir DDRB			
+#define LCD_Data_Port PORTB				
+#define LCD_Command_Port PORTB			
+#define EN PB0							
+#define RW PB1							
+#define RS PB2							
+
+void LCD_Command (char);				
+void LCD_Char (char);					
+void LCD_Init (void);					
+void LCD_String (char*);			
+void LCD_String_xy (char,char,char*);	
+void LCD_Clear (void);				
+
+
+#endif									
